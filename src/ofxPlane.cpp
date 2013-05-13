235781d9a48f732e91e56924e79d24dc2cf5c2e2
Tyler Parker <btylerparker@gmail.com>
2013-05-12 20:29:15 -0400

    Least fit plane, now to test some more

--- src/ofxPlane.cpp
+++ src/ofxPlane.cpp
@@ -26,26 +26,105 @@ ofxPlane::ofxPlane(ofxLine3d line0, ofxLine3d line1){
 		initialized = false;
 	}
 	else{
-		// Computing 'good enough' fit plane
-		ofPoint pt0 = line0.pt;
-		ofPoint pt1 = line1.pt;
-		ofPoint pt2 = (line0.pt+line0.dir);
+		ofPoint pts[4];
+		pts[0] = line0.pt;
+		pts[1] = line1.pt;
+		pts[2] = (line0.pt+line0.dir);
+		pts[3] = (line1.pt+line1.dir);
+		
 
-		ofVec3f pt01 = pt1-pt0;
-		ofVec3f pt02 = pt2-pt0;
+		
+		float sum_i_x = 0;
+		float sum_i_xx = 0;
+		float sum_i_xy = 0;
+		float sum_i_yy = 0;
+		float sum_i_y = 0;
+		float sum_i_xz = 0;
+		float sum_i_yz = 0;
+		float sum_i_z = 0;
 
-		ofVec3f normalVec = pt01.cross(pt02);
-		normalVec.normalize();
+		int n = 4;
 
-		setNormal(normalVec.x,normalVec.y,normalVec.z);
-		cout << "Line Crossed Plane normal " << endl;
-		cout << normal << endl;
-		setPoint(pt2.x,pt2.y,pt2.z);
+		for(int i = 0; i < n; i++){
+			sum_i_x 	+= 	pts[i].x;
+			sum_i_xx 	+= 	pts[i].x * pts[i].x;
+			sum_i_xy 	+= 	pts[i].x * pts[i].y;
+			sum_i_yy 	+= 	pts[i].y * pts[i].y;
+			sum_i_y 	+= 	pts[i].y;
+			sum_i_xz 	+= 	pts[i].x * pts[i].z;
+			sum_i_yz 	+= 	pts[i].y * pts[i].z;
+			sum_i_z 	+= 	pts[i].z;
+		}
+
+		/*
+		If you have n data points (x[i], y[i], z[i]), compute the 3x3 symmetric matrix A whose entries are:
+
+		sum_i x[i]*x[i],    sum_i x[i]*y[i],    sum_i x[i]
+		sum_i x[i]*y[i],    sum_i y[i]*y[i],    sum_i y[i]
+		sum_i x[i],         sum_i y[i],         n
+
+		*/
+		CvMat* A = cvCreateMat( 3, 3, CV_32FC1 );
+		CvMat* invA = cvCreateMat( 3, 3, CV_32FC1 );
+		CV_MAT_ELEM(*A,float,0,0) = sum_i_xx;	CV_MAT_ELEM(*A,float,0,1) = sum_i_xy;	CV_MAT_ELEM(*A,float,0,2) = sum_i_x;	
+		CV_MAT_ELEM(*A,float,1,0) = sum_i_xy;	CV_MAT_ELEM(*A,float,1,1) = sum_i_yy;	CV_MAT_ELEM(*A,float,1,2) = sum_i_y;	
+		CV_MAT_ELEM(*A,float,2,0) = sum_i_x;	CV_MAT_ELEM(*A,float,2,1) = sum_i_y;	CV_MAT_ELEM(*A,float,2,2) = (float)n;	
+
+
+		/*
+		Also compute the 3 element vector b:
+
+		{sum_i x[i]*z[i],   sum_i y[i]*z[i],    sum_i z[i]}
+
+		*/
+
+		CvMat* b = cvCreateMat( 3,1, CV_32FC1 );
+		CV_MAT_ELEM(*b,float,0,0) = sum_i_xz;	CV_MAT_ELEM(*b,float,1,0) = sum_i_yz;	CV_MAT_ELEM(*b,float,2,0) = sum_i_z;	
+
+		CvMat* x  = cvCreateMat(3,1,CV_32FC1);
+
+		cvInv(A,invA);
+
+		cvMatMul(invA,b,x);
+
+		//cvSolve(&A, &b, &x, CV_LU);    // solve (Ax=b) for x
+
+		float planeA = CV_MAT_ELEM(*x,float,0,0);
+		float planeB = CV_MAT_ELEM(*x,float,1,0);
+		float planeC = -1;
+		float planeD = -1*CV_MAT_ELEM(*x,float,2,0);
+
+		float z = (-planeD/planeC);
+
+		ofPoint pointRes = ofPoint(0,0,z);
+
+		ofxPlane planeRes;
+
+		ofVec3f normalRes = ofVec3f(planeA,planeB,planeC);
+		normalRes.normalize();
+
+
+
+		/*
+		Then solve Ax = b for the given A and b. The three components of the solution vector are the 
+		coefficients to the least-square fit plane {a,b,c}.
+
+		Note that this is the "ordinary least squares" fit, which is appropriate only when z is expected to 
+		be a linear function of x and y. If you are looking more generally for a "best fit plane" in 3-space, 
+		you may want to learn about "geometric" least squares.
+
+		Note also that this will fail if your points are in a line, as your example points are.
+
+
+		*/
+
+
+
+		setNormal(normalRes.x,normalRes.y,normalRes.z);
+		setPoint(pointRes.x,pointRes.y,pointRes.z);
 		computeD();
 		initialized = true;
 	}
-	
-	
 }
 
 
